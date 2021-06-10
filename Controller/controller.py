494b07bae0a797c922d3037e60cc490035d37192
Davis Kerr <daviskerr@byui.edu>
2021-06-10 14:12:11 -0600

    Commit files

---
+++ Controller/controller.py
@@ -0,0 +1,7 @@
+from View.capture_screen import Capture_Screen
+
+
+class Controller:
+
+    def __init__(self):
+        screen = Capture_Screen(lambda x: x)
---
+++ main.py
@@ -0,0 +1,8 @@
+from Controller.controller import Controller
+from View.capture_screen import Capture_Screen
+from tkinter import *
+from View.double_slider import Double_Slider
+import cv2 as cv
+
+if __name__ == "__main__":
+    controller = Controller()
