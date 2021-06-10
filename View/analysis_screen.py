107bcba8447a6446b565333b3650af0c0c94a0aa
jrperlic <jperlic@byui.edu>
2021-06-10 14:41:01 -0600

    setting up analysis screen

--- View/analysis_screen.py
+++ View/analysis_screen.py
@@ -4,7 +4,7 @@ from PIL import ImageTk, Image
 from tkvideo import tkvideo
 from tkinter import filedialog
 
-class Analysis_Screen:
+class Analysis_Screen(filename):
 
     def __init__(self, slider_function):
         self.window = Tk()
@@ -12,39 +12,29 @@ class Analysis_Screen:
         self.window.iconbitmap("Images/Golf.ico")
         self.slider_function = slider_function
 
-        self.filename = ""
-
         self.build_window()
 
         self.window.mainloop()
     
-    def get_filename(self):
-        """Open a filedialog for File."""
-        return filedialog.askopenfilename()
-    
     def load_video(self):
-        self.filename = self.get_filename()
         self.video_player = tkvideo(self.filename, self.video_display, loop=1, size=(400,400))
         self.video_player.play()
 
 
     def build_window(self):
         self.title = Label(self.window, text="Record and trim golf stroke video")
-
-        #self.my_img = ImageTk.PhotoImage(Image.open('Images/placeholder.jpg'))
         
         self.video_display = Label(self.window)
-        #self.player = tkvideo("swing01_Trim.mp4", self.video_display, loop=1, size=(400,400))
-        #self.player.play()
  
         self.play_btn = Button(self.window, text="Play")
         self.pause_btn = Button(self.window, text="Pause")
         self.record_btn = Button(self.window, text="Record")
         self.video_slider = Double_Slider(self.window, lambda x : x)
         self.submit_btn = Button(self.window, text="Submit Selection")
-        self.btnFile = Button(self.window, text="File", command=self.load_video)
+        self.btnView = Button(self.window, text="View")
+        self.readout = Text(self.window)
 
-        self.btnFile.grid(row=0, column=0)
+        self.btnView.grid(row=0, column=0)
         self.title.grid(row=1, column=3)
         self.video_display.grid(row=1, column=1, rowspan=3, columnspan=5)
         self.play_btn.grid(row=6, column=2)
@@ -52,4 +42,5 @@ class Analysis_Screen:
         self.record_btn.grid(row=6, column=4)
         self.video_slider.grid(row=7, column=1, columnspan=5)
         self.submit_btn.grid(row=8, column=3)
+        self.readout.grid(row=9, column=1, columnspan=5)
 
